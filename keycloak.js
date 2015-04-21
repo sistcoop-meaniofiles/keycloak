acb6474d14a4d9b00f4083951a11989bba4c0cdd
carlosthe19916 <carlosthe19916@gmail.com>
2015-04-21 17:58:56 -0500

    MongoStore

--- keycloak.js
+++ keycloak.js
@@ -3,9 +3,15 @@
 var Keycloak = require('connect-keycloak');
 var express = require('express');
 var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var mongoose = require('mongoose');
+
+//app
 var app = express();
 
-var memoryStore = new session.MemoryStore();
+//store
+var connection = mongoose.createConnection();
+var memoryStore = new MongoStore({ mongooseConnection: connection });
 
 app.use( session({
     secret: '2fafb62e-3437-4bc3-b1cb-42f591d85c8f',
