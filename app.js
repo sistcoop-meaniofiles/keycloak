c95105f0da1f5c27236b6df554cf46421d9c6576
Carlos <carlosthe19916@gmail.com>
2015-04-24 12:47:34 -0500

    keycloak.json ubicacion correcta

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ Keycloak.register(function(app, auth, database) {
 
     //add libraries
     Keycloak.aggregateAsset('js', '../lib/keycloak/dist/keycloak.js', {global:true,  weight: -4, group: 'header'});
-    Keycloak.aggregateAsset('js', '../js/keycloak.js', {global:true,  weight: -3, group: 'footer'});
 
     return Keycloak;
 });
