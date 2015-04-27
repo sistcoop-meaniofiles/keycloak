4ae69a46bc76bee9142a314c0d3daa0f55d90e40
Carlos <carlosthe19916@gmail.com>
2015-04-27 12:51:47 -0500

    remove alert

--- public/controllers/KeycloakLogoutController.js
+++ public/controllers/KeycloakLogoutController.js
@@ -3,7 +3,6 @@
 /* jshint -W098 */
 angular.module('mean.keycloak').controller('KeycloakLogoutController', ['$scope', 'Auth',
     function($scope, Auth) {
-        alert('entro');
         Auth.authz.logout();
     }
 ]);
