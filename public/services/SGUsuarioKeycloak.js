0eb809349f8072cf300acb1b91a2bbd5f6d6fe5a
Carlos <carlosthe19916@gmail.com>
2015-05-11 11:32:45 -0500

    add SGUsuario

---
+++ public/services/SGUsuarioKeycloak.js
@@ -0,0 +1,47 @@
+'use strict';
+
+angular.module('mean.keycloak').provider('sgKeycloak', function() {
+
+    this.restUrl = 'http://localhost';
+
+    this.$get = function() {
+        var restUrl = this.restUrl;
+        return {
+            getRestUrl: function() {
+                return restUrl;
+            }
+        }
+    };
+
+    this.setRestUrl = function(restUrl) {
+        this.restUrl = restUrl;
+    };
+});
+
+
+angular.module('mean.keycloak').factory('KeycloakRestangular', ['Restangular', 'sgKeycloak', function(Restangular, sgKeycloak) {
+    return Restangular.withConfig(function(RestangularConfigurer) {
+        RestangularConfigurer.setBaseUrl(sgKeycloak.getRestUrl());
+    });
+}]);
+
+angular.module('mean.keycloak').factory('SGUsuario', ['KeycloakRestangular',  function(KeycloakRestangular) {
+
+    var url = "users";
+
+    var modelMethos = {
+        $find: function(username){
+            return KeycloakRestangular.one(url, username).get();
+        },
+        $search: function(queryParams){
+            return KeycloakRestangular.all(url).getList(queryParams);
+        },
+        $roleMappings : function(username){
+            return KeycloakRestangular.one(url + '/' + username + '/role-mappings').get();
+        }
+
+    };
+
+    return modelMethos;
+
+}]);
