c5d845c404784696fa9bc14547b51b70a45497ea
Carlos <carlosthe19916@gmail.com>
2015-05-11 15:58:52 -0500

    mostrar rol en cabecera

--- public/controllers/KeycloakAccountManagementController.js
+++ public/controllers/KeycloakAccountManagementController.js
@@ -1,11 +1,25 @@
 'use strict';
 
 /* jshint -W098 */
-angular.module('mean.keycloak').controller('KeycloakAccountManagementController', ['$scope', 'Auth',
-    function($scope, Auth) {
+angular.module('mean.keycloak').controller('KeycloakAccountManagementController', ['$scope', 'Auth', 'SGUsuarioKeycloak',
+    function($scope, Auth, SGUsuarioKeycloak) {
         //Auth.authz.accountManagement();
         $scope.user = {
-            username: Auth.authz.idTokenParsed.preferred_username
+            username: Auth.authz.idTokenParsed.preferred_username,
+            roles: []
         };
+        
+        $scope.loadRoles = function(){
+            SGUsuarioKeycloak.$roleMappings($scope.user.username).then(function(response){
+                var realmRoles = response.realmMappings;
+                for(var i=0; i<realmRoles.length; i++){
+                    if(realmRoles[i].name !== 'manage-account' && realmRoles[i].name !== 'view-profile'){
+                        $scope.user.roles.push(realmRoles[i].name);
+                    }
+                }
+            });
+        };
+        $scope.loadRoles();
+
     }
 ]);
