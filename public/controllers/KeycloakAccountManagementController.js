184c4e73d7827cf2bb72f59b79ffa31c800dfd83
Carlos <carlosthe19916@gmail.com>
2015-05-13 16:28:00 -0500

    header

--- public/controllers/KeycloakAccountManagementController.js
+++ public/controllers/KeycloakAccountManagementController.js
@@ -1,8 +1,8 @@
 'use strict';
 
 /* jshint -W098 */
-angular.module('mean.keycloak').controller('KeycloakAccountManagementController', ['$scope', 'Auth', 'SGUsuarioKeycloak', 'SGSession',
-    function ($scope, Auth, SGUsuarioKeycloak, SGSession) {
+angular.module('mean.keycloak').controller('KeycloakAccountManagementController', ['$scope', 'Auth', 'SGUsuarioKeycloak', 'SGSession', 'toastr',
+    function ($scope, Auth, SGUsuarioKeycloak, SGSession, toastr) {
 
         $scope.user = {
             username: Auth.authz.idTokenParsed.preferred_username,
@@ -21,6 +21,20 @@ angular.module('mean.keycloak').controller('KeycloakAccountManagementController'
                 for (var i = 0; i < response.length; i++) {
                     $scope.user.roles.push(response[i].name);
                 }
+
+                //verificar si el rol CAJERO tiene una caja asignada
+                if($scope.user.roles.indexOf('CAJERO') != -1) {
+                    var listener = $scope.$watch('session.caja', function(newValue) {
+                        if(angular.isUndefined(newValue)){
+                            toastr.error('CAJA no cargada. Podria ocasionar un mal funcionamiento del sistema', '', {
+                                closeButton: true,
+                                timeOut: 0,
+                                positionClass: 'toast-top-center'
+                            });
+                        }
+                    }, true);
+                }
+
             });
         };
         $scope.loadRoles();
@@ -30,6 +44,13 @@ angular.module('mean.keycloak').controller('KeycloakAccountManagementController'
         $scope.loadSucursal = function () {
             SGSession.getSucursal().then(function (response) {
                 $scope.session.sucursal = response;
+                if(angular.isUndefined($scope.session.sucursal)){
+                    toastr.error('SUCURSAL no cargada. Podria ocasionar un mal funcionamiento del sistema', '', {
+                        closeButton: true,
+                        timeOut: 0,
+                        positionClass: 'toast-top-center'
+                    });
+                }
             });
         };
         $scope.loadSucursal();
@@ -37,6 +58,13 @@ angular.module('mean.keycloak').controller('KeycloakAccountManagementController'
         $scope.loadAgencia = function () {
             SGSession.getAgencia().then(function (response) {
                 $scope.session.agencia = response;
+                if(angular.isUndefined($scope.session.agencia)){
+                    toastr.error('AGENCIA no cargada. Podria ocasionar un mal funcionamiento del sistema', '', {
+                        closeButton: true,
+                        timeOut: 0,
+                        positionClass: 'toast-top-center'
+                    });
+                }
             });
         };
         $scope.loadAgencia();
@@ -44,6 +72,13 @@ angular.module('mean.keycloak').controller('KeycloakAccountManagementController'
         $scope.loadTrabajadorCaja = function () {
             SGSession.getTrabajadorCaja().then(function (response) {
                 $scope.session.trabajadorCaja = response;
+                if(angular.isUndefined($scope.session.trabajadorCaja)){
+                    toastr.error('TRABAJADOR no cargado. Podria ocasionar un mal funcionamiento del sistema', '', {
+                        closeButton: true,
+                        timeOut: 0,
+                        positionClass: 'toast-top-center'
+                    });
+                }
             });
         };
         $scope.loadTrabajadorCaja();
